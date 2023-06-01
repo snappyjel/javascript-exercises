60babdd35b347116e1f7570b679949324472634d
snappyjel <leviftr1@gmail.com>
2023-06-01 19:03:45 +0700

    Reverse array

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function (word) {
+  let split = word.split("");
+  let reverseArray = split.reverse();
+  let joinArray = reverseArray.join("");
+  return joinArray;
 };
 
 // Do not edit below this line
