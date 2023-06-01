b8f6c284cbe63fe543d657e20dcd9c5eec19843a
snappyjel <leviftr1@gmail.com>
2023-06-01 22:04:06 +0700

    Create function to remove from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...number) {
+  const newArray = [];
+  array.forEach((object) => {
+    if (!number.includes(object)) {
+      newArray.push(object);
+    }
+  });
+  return newArray;
 };
 
 // Do not edit below this line
