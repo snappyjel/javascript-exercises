d4bf89e7c16eafad01b804b9500dd119afc5f526
snappyjel <leviftr1@gmail.com>
2023-06-02 23:00:34 +0700

    Function to convert temp

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (DegreeC) {
+  let celcius = ((DegreeC - 32) * 5) / 9;
+  const roundedCelcius = Math.round(celcius * 10) / 10;
+  return roundedCelcius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (DegreeF) {
+  let fahrenheit = (DegreeF * 9) / 5 + 32;
+  const roundedFahrenheit = Math.round(fahrenheit * 10) / 10;
+  return roundedFahrenheit;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
