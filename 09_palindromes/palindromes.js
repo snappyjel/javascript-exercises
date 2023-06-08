c0b8c5d24cdf87c2954e27e502c91fad30b3751c
snappyjel <leviftr1@gmail.com>
2023-06-08 21:33:45 +0700

    Palindrome checker

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  str = str.toLowerCase().replace(/[^a-z0-9]/g, '')
+  let reverseStr = str.split("").reverse().join("");
+  return str === reverseStr;
 };
 
 // Do not edit below this line
